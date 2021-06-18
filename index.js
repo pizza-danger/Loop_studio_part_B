da6e19a984fa795077a92de451cc8fa26a85dcb3
thereyner <33945338+pizza-danger@users.noreply.github.com>
2021-06-18 14:41:04 +0000

    getting close

--- index.js
+++ index.js
@@ -4,5 +4,26 @@
 
 //For now, make sure the two strings contain the same number of characters!
 
-let  = 'Rutabagas';
-let  = 'Chemistry';
+let string1 = 'Rutabagas';
+let string2 = 'Chemistry';
+let string1Arr = string1.split("");
+let string2Arr = string2.split("");
+let newStringArr = [];
+let newString = "";
+
+for (let i = 0; i <= string1.length + string2.length; i++) {
+  newStringArr.push(string1Arr[i]+string2Arr[i]);
+  newString = newStringArr.join("");
+}
+
+console.log(newString);
+//console.log(string1Arr);
+// let str1Arr = [];
+// let str2Arr = [];
+// let str = [];
+
+// string1.split("");
+// string2.split();
+
+
+//  console.log(str);
